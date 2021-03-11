8ba50391ea31fe745603d7ff4daf5b3d9da09681
julia <juliazoratti@gmail.com>
2021-03-11 18:19:47 -0300

    Lista 1

--- Atividade 2.sql
+++ Atividade 2.sql
@@ -1,35 +1,32 @@
 -- criando banco de dados
-CREATE DATABASE db_escola;
+CREATE DATABASE db_informacoes;
 
 -- informando o banco de dados
-USE db_escola;
+USE db_informacoes;
 
 -- criando tabela
-CREATE TABLE tb_alunos (
-id BIGINT (8) AUTO_INCREMENT,
+CREATE TABLE tb_funcionarios (
+id BIGINT (5) AUTO_INCREMENT,
 nome VARCHAR (100),
-professor VARCHAR (20),
-serie VARCHAR (30),
-nota DECIMAL (10,2),
+cpf VARCHAR (20),
+funcao VARCHAR (30),
+salario BIGINT (10),
 
 PRIMARY KEY (id)
 );
 
 -- populando tabela 
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Juliana", "Carlos", "5a - B", 10.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Carla", "Carlos", "5a - B", 2.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Luiz", "Maria", "5a - A", 5.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Roberto", "Carlos", "5a - B", 4.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Carolina", "Maria", "5a - A", 7.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Daniel", "Maria", "5a - A", 8.00);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Tatiana", "Carlos", "5a - B", 5.25);
-INSERT INTO tb_alunos (nome, professor, serie, nota) VALUE ("Caio", "Maria", "5a - A", 10.00);
+INSERT INTO tb_funcionarios (nome, cpf, funcao, salario) VALUE ("Rodrigo", "111.111.111-11", "Recepcionista", 1000);
+INSERT INTO tb_funcionarios (nome, cpf, funcao, salario) VALUE ("Roberto", "222.222.222-22", "Atendimento ao cliente", 2000);
+INSERT INTO tb_funcionarios (nome, cpf, funcao, salario) VALUE ("Maria Fernanda", "333.333.333-33", "TI", 3000);
+INSERT INTO tb_funcionarios (nome, cpf, funcao, salario) VALUE ("Fábia", "444.444.444-44", "Administração", 4000);
+INSERT INTO tb_funcionarios (nome, cpf, funcao, salario) VALUE ("Jéssica", "555.555.555-55", "Marketing", 5000);
 
--- notass maiores que 7,00
-SELECT * FROM tb_alunos WHERE nota > 7.00;
+-- salarios maiores que 2000
+SELECT * FROM tb_funcionarios WHERE salario > 2000;
 
--- notas menores que 7,00
-SELECT * FROM tb_alunos WHERE preco < 7.00;
+-- salarios menores que 2000
+SELECT * FROM tb_funcionarios WHERE salario < 2000;
 
 -- atualizando dado de uma query
-UPDATE tb_aulos SET marca = "C&A" WHERE id = 7;
+UPDATE tb_funcionarios SET cpf = "111.222.333-44" WHERE id = 3;
